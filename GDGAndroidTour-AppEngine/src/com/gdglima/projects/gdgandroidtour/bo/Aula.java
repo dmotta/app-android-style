69cb8df5951ee37221cd9cd2d89d5fa1f3f70bdd
dmotta <mdavid20@gmail.com>
2013-03-08 18:56:52 -0500

    Banckend of App GDGAndroidTour

---
+++ GDGAndroidTour-AppEngine/src/com/gdglima/projects/gdgandroidtour/bo/Aula.java
@@ -0,0 +1,36 @@
+package com.gdglima.projects.gdgandroidtour.bo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Aula {
+	
+	@Id
+	private Long id;
+	private String nombre;
+	
+	
+	public Aula() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
